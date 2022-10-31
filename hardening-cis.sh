232ae86a945cda0ff72c26c5987b62fce889879a
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-10-31 14:20:17 +0800

    Update hardening-cis.sh

--- hardening-cis.sh
+++ hardening-cis.sh
@@ -366,7 +366,7 @@ cmd59=$(printf " -a always,exit -F arch=b32 -S sethostname,setdomainname -k syst
 cmd60=$(augenrules --load)
 cmd61=$(bash 4-1-3-6.sh)
 cmd62=$(augenrules --load)
-cmd63=$(bash 4-1-3-7.sh)
+cmd63=$(bash 4-1-3-7.sh) #does not work
 #{UID_MIN=$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs) [ -n "${UID_MIN}" ] && printf " -a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EACCES -F auid>=${UID_MIN} -F auid!=unset -k access\n -a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EPERM -F auid>=${UID_MIN} -F auid!=unset -k access" >> /etc/audit/rules.d/50-access.rules || printf "ERROR: Variable 'UID_MIN' is unset.\n"})
 cmd64=$(augenrules --load)
 cmd65=$(bash 4-1-3-8.sh)
