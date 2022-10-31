870685d8f3571f2eee1abfe27bd121733ab0309d
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-10-31 10:57:38 +0800

    Update 4-1-3-7.sh

--- 4-1-3-7.sh
+++ 4-1-3-7.sh
@@ -1,2 +1,4 @@
 UID_MIN=$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)
 [ -n "${UID_MIN}" ] && printf " -a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EACCES -F auid>=${UID_MIN} -F auid!=unset -k access \n -a always,exit -F arch=b32 -S creat,open,openat,truncate,ftruncate -F exit=-EPERM -F auid>=${UID_MIN} -F auid!=unset -k access">> /etc/audit/rules.d/50-access.rules || printf "ERROR: Variable 'UID_MIN' is unset.\n"}
+
+augenrules --load
