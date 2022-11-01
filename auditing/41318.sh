cb3551de9ce9d21b9b28023fc367fa9b9ea09a51
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-11-01 12:54:09 +0800

    Add files via upload

---
+++ auditing/41318.sh
@@ -0,0 +1,6 @@
+{
+UID_MIN=$(awk '/^\s*UID_MIN/{print $2}' /etc/login.defs)
+[ -n "${UID_MIN}" ] && printf "
+-a always,exit -F path=/usr/sbin/usermod -F perm=x -F auid>=${UID_MIN} -F auid!=unset -k usermod
+" >> /etc/audit/rules.d/50-usermod.rules
+}
