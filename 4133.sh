79ebdff6a2e1de68ab8034bbe57f80b74d23c500
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-11-02 10:29:42 +0800

    Delete 4133.sh

--- 4133.sh
+++
@@ -1,4 +0,0 @@
-{
-SUDO_LOG_FILE=$(grep -r logfile /etc/sudoers* | sed -e 's/.*logfile=//;s/,?.*//' -e 's/"//g')
-[ -n "${SUDO_LOG_FILE}" ] && printf " -w ${SUDO_LOG_FILE} -p wa -k sudo_log_file" >> /etc/audit/rules.d/50-sudo.rules || printf "ERROR: Variable 'SUDO_LOG_FILE_ESCAPED' is unset.\n"
-}
