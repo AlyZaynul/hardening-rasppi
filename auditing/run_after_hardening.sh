9e96172bf4a7decf409359ba19500e9f132b31dd
AlyZaynul <111562251+AlyZaynul@users.noreply.github.com>
2022-11-03 15:55:58 +0800

    Update run_after_hardening.sh

--- auditing/run_after_hardening.sh
+++ auditing/run_after_hardening.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 cmd1=$(bash 4131.sh)
 cmd2=$(bash 4132.sh)
 #.3 has errors
@@ -18,3 +19,42 @@ cmd15=$(bash 41317.sh)
 cmd16=$(bash 41318.sh)
 #.19 not for 32bit system
 cmd17=$(bash 41320.sh)
+
+
+#4.1.4.1
+cmd87=$(chmod 640 /etc/audit/auditd.conf)
+cmd88=$(chown root /etc/audit/auditd.conf)
+cmd89=$(chgrp adm /var/log/audit/)
+cmd90=$(sed -ri 's/^\s*#?\s*log_group\s*=\s*\S+(\s*#.*)?.*$/log_group = adm\1/' /etc/audit/auditd.conf)
+cmd91=$(systemctl restart auditd)
+#4.1.4.4
+cmd92=(chmod g-w,o-rwx "$(dirname $(awk -F"=" '/^\s*log_file/ {print $2}' /etc/audit/auditd.conf))")
+
+#cmd92=$(chmod 0750 /var/log/audit)
+
+#4.1.4.5
+cmd93=$(find /etc/audit/ -type f \( -name '*.conf' -o -name '*.rules' \) -exec chmod u-x,g-wx,o-rwx {} +)
+
+
+#4.1.4.6
+cmd94=$(find /etc/audit/ -type f \( -name '*.conf' -o -name '*.rules' \) ! -user root -exec chown root {} +)
+
+#4.1.4.7
+cmd95=$(find /etc/audit/ -type f \( -name '*.conf' -o -name '*.rules' \) ! -group root -exec chgrp root {} +)
+
+#4.1.4.8
+cmd96=$(chmod go-w /sbin/auditctl /sbin/aureport /sbin/ausearch /sbin/autrace /sbin/auditd /sbin/augenrules)
+
+#4.1.4.9 
+cmd97=$(chown root /sbin/auditctl /sbin/aureport /sbin/ausearch /sbin/autrace /sbin/auditd /sbin/augenrules)
+
+#4.1.4.10
+cmd98=$(chmod go-w /sbin/auditctl /sbin/aureport /sbin/ausearch /sbin/autrace /sbin/auditd /sbin/augenrules)
+cmd99=$(chown root:root /sbin/auditctl /sbin/aureport /sbin/ausearch /sbin/autrace /sbin/auditd /sbin/augenrules)
+
+#4.1.4.11 As aide is not supported by rasp pi this step is skipped
+
+#4.2.1.1.1
+#apt install systemd-journal-remote NOT installed as there is not dedicated remote log host
+
+
